49679dbc9a608f3ace147a94755a2fd2387fdeb3
mavisliu689 <r03241304@ntu.edu.tw>
2017-12-06 17:59:44 +0800

    vue instance example

--- Vue Instance/app.js
+++ Vue Instance/app.js
@@ -1,5 +1,12 @@
-new Vue({
-  el: '#app',
+var data = {
+  title: 'The VueJS Instance',
+  showParagraph: false
+}
+
+Vue.component('www',{
+  template: '<h2>this is www section!</h2>'
+})
+var vm1 = new Vue({
   data: {
     title: 'The VueJS Instance',
     showParagraph: false
@@ -8,6 +15,7 @@ new Vue({
     show: function() {
       this.showParagraph = true;
       this.updateTitle('The VueJS Instance (Updated)');
+      this.$refs.myButton.innerText = 'Text'
     },
     updateTitle: function(title) {
       this.title = title;
@@ -24,3 +32,64 @@ new Vue({
     }
   }
 });
+
+vm1.$mount("#app1")
+
+console.log(vm1.$data === data);
+vm1.$refs.heading.innerText = "Somethings else";
+//太吵了先關掉
+// setTimeout(function(){
+//   vm1.title = 'changed by timer';
+// }, 3000)
+var vm2 = new Vue({
+  el: '#app2',
+  data: {
+    title: 'The app2'
+  },
+  methods: {
+    onChange: function(){
+      vm1.title = 'change'
+    }
+  }
+})
+
+
+var vm3 = new Vue({
+  el: '.hello',
+  template: '<h1>Hello!</h1>'
+})
+
+var vm4 = new Vue({
+  el: '#app5',
+  data: {
+    title: 'The VueJS Instance'
+  },
+  beforeCreate: function() {
+     console.log('beforeCreate()')
+  },
+  created: function(){
+    console.log('created()')
+  },
+  beforeMount: function() {
+    console.log('beforeMount()')
+  },
+  beforeUpdate: function() {
+    console.log('beforeUpdate()')
+  },
+  updated: function(){
+    console.log('updated()')
+  },
+  beforeDestory: function(){
+    console.log('beforeDestory()')
+  },
+  destroyed: function(){
+    console.log('destroyed()')
+  },
+  methods: {
+  	destroy: function() {
+    	this.$destroy();
+    }
+  }
+})
+// vm3.$mount();
+// document.getElementById('app3').appendChild(vm3.$el);
