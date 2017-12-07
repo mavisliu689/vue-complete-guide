964f23e1260ab4dd6adf3a53365993e7c4ec4c22
mavisliu689 <r03241304@ntu.edu.tw>
2017-12-07 12:06:45 +0800

    7-hw done

--- 7-hw/src/App.vue
+++ 7-hw/src/App.vue
@@ -1,39 +1,30 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
+        <headerr></headerr>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
+            <left-content></left-content>
+            <right-content></right-content>
         </div>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <footerr></footerr>
     </div>
 </template>
 
 <script>
+    import headerr from './header.vue';
+    import footerr from './footer.vue';
+    import leftContent from './leftContent.vue';
+    import rightContent from './rightContent.vue';
+
+    export default {
+        components:{
+            headerr,
+            footerr,
+            leftContent,
+            rightContent
+        }
+    }
 </script>
 
 <style>
