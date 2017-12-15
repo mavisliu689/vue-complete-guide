7fb68f8bdbd3383e964d04d1c554596b309ebe78
mavisliu689 <r03241304@ntu.edu.tw>
2017-12-15 18:03:29 +0800

    filter done

---
+++ filter and mixins/src/List.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits" :key="fruit">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { fruitMixin} from './fruitMixin'
+    export default {
+        mixins: [fruitMixin],
+        created() {
+            console.log("Inside List Created Hook!");
+        }
+    }
+</script>
+
+<style>
+
+</style>
