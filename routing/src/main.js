a225d0901de7fd1ebbb7d9d222c79628bd4c010c
mavisliu689 <r03241304@ntu.edu.tw>
2017-12-20 18:12:58 +0800

    add router

--- routing/src/main.js
+++ routing/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+import { routes } from './router';
+Vue.use(VueRouter);
 
+const router = new VueRouter({
+  mode: 'history',
+  routes: routes
+});
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
