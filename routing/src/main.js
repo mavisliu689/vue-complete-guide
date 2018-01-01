387af82206cbfb6c63bc85a74acc579afff14ac1
mavis <r03241304@ntu.edu.tw>
2018-01-01 16:22:15 +0800

    router done

--- routing/src/main.js
+++ routing/src/main.js
@@ -6,7 +6,23 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: 'history',
-  routes: routes
+  routes: routes,
+  scrollBejavior(to, from, savedPosition) {
+    if (savedPosition){
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    // return {x:0, y:700};
+  }
+});
+
+router.beforEach((to, from, next) => {
+  console.log('global beforeach');
+  next({
+    
+  });
 });
 new Vue({
   el: '#app',
--- routing/src/router.js
+++ routing/src/router.js
@@ -1,9 +1,31 @@
 import User from './components/user/User.vue';
 import Home from './components/Home.vue';
 import Header from './components/Header.vue';
-import UserStart from './components/user/UserStart.vue';
-import UserDetail from './components/user/UserDetail.vue';
-import UserEdit from './components/user/UserEdit.vue';
+// import UserStart from './components/user/UserStart.vue';
+// import UserDetail from './components/user/UserDetail.vue';
+// import UserEdit from './components/user/UserEdit.vue';
+
+const User = resolve => {
+    require.ensure(['./component/User/User.vue'], () => {
+        resolve(require('./component/User/User.vue'))
+    })
+}
+const UserStart = resolve => {
+    require.ensure(['./component/User/UserStart.vue'], () => {
+        resolve(require('./component/User/UserStart.vue'))
+    })
+}
+const UserDetail = resolve => {
+    require.ensure(['./component/User/UserDetail.vue'], () => {
+        resolve(require('./component/User/UserDetail.vue'))
+    })
+}
+const UserEdit = resolve => {
+    require.ensure(['./component/User/UserEdit.vue'], () => {
+        resolve(require('./component/User/UserEdit.vue'))
+    })
+}
+
 export const routes = [
     {path: '',
         component: Home,
@@ -19,7 +41,9 @@ export const routes = [
         },
         children: [
             {path: '', component: UserStart, name: 'userStart'},
-            {path: ':id', component: UserDetail, name: 'userDetail'},
+            {path: ':id', component: UserDetail, name: 'userDetail', beforeEnter: (to, from, next) => {
+                next();
+            }},
             {path: ':id/edit', component: UserEdit, name: 'userEdit'}
         ]
     },
