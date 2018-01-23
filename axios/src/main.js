78bdadacf83dc436f98a68ba37a81e5322cb606a
mavisliu689 <r03241304@ntu.edu.tw>
2018-01-23 14:34:11 +0800

    axios done

---
+++ axios/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+
+import router from './router'
+import store from './store'
+
+axios.defaults.baseURL = 'https://vue-update-368f6.firebaseio.com/';
+axios.defaults.headers.common['Authorization'] = 'dfdfd';
+axios.defaults.headers.get['Accept'] = 'application/json'
+
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log("request", config)
+  return config
+});
+
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log("response",res)
+  return res
+})
+
+axios.interceptors.request.eject(reqInterceptor)
+axios.interceptors.response.eject(resInterceptor)
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
